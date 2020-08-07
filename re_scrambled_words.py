160c172ac664c145f1e0293e14c7d3f298223620
shaman-kai <69352832+shaman-kai@users.noreply.github.com>
2020-08-07 19:47:48 +0200

    Update re_scrambled_words.py

--- re_scrambled_words.py
+++ re_scrambled_words.py
@@ -7,7 +7,8 @@ import random
 
 def shuffled_middle_word(m):
     """
-    This function takes a result of string patterning consisting of groups (e.g. beginning of string + middle + end), shuffles the second group and returns a string joining 1 group + shuffled 2nd group + 3rd group. Intended use: to get a string with shuffled middle.
+    This function takes a result of string patterning consisting of groups (e.g. beginning of string + middle + end), shuffles the second group 
+    and returns a string joining 1 group + shuffled 2nd group + 3rd group. Intended use: to get a string with shuffled middle.
         INPUT AND OUTPUT:
         In: Takes a single input argument of type re.Match.
         Out: Returns a single string.
@@ -24,7 +25,8 @@ def shuffled_middle_word(m):
     inner_word = list(m.group(2))
     random.shuffle(inner_word)
     return m.group(1) + "".join(inner_word) + m.group(3)
-    #(if necessary, the segmentation of a word can be changed to any number of segments (not necessarily 3); in that case, the pattern in shuffle_middles_re should be changed accordingly)
+    #(if necessary, the segmentation of a word can be changed to any number of segments (not necessarily 3); in that case, 
+    # the pattern in shuffle_middles_re should be changed accordingly)
  
     
 def shuffled_word(m):
@@ -46,7 +48,8 @@ def shuffled_word(m):
     word = list(m.group(0))
     random.shuffle(word)
     return "".join(word)
-    #(if necessary, this function can be changed to perform other operations instead of shuffling by writing something else instead of random.shuffle(word). Results of main functions shuffle_words_re and shuffle_short_words_re will change accordingly)
+    #(if necessary, this function can be changed to perform other operations instead of shuffling by writing something else instead of random.shuffle(word). 
+    # Results of main functions shuffle_words_re and shuffle_short_words_re will change accordingly)
 
 #%% Main functions
      
@@ -68,7 +71,8 @@ def shuffle_middles_re(text):
         raise TypeError("The input argument should be a piece of text as a string")
         
     return re.sub(r"(\w)(\w+)(\w)", shuffled_middle_word, text)
-    # (if necessary, the pattern first letter + middle + last letter (\w)(\w+)(\w) can be substituted by any other pattern: with a different number of initial and/or end letters, different word segmentation (e.g.in half) or a different sentence segmentation)
+    # (if necessary, the pattern first letter + middle + last letter (\w)(\w+)(\w) can be substituted by any other pattern: with a different number of 
+    # initial and/or end letters, different word segmentation (e.g.in half) or a different sentence segmentation)
 
 
 def shuffle_words_re(text):
@@ -97,7 +101,8 @@ def shuffle_short_words_re(text, n):
     This function takes a piece of text and randomly reorders all letters in words that are shorter than or equal to a given number of letters (<=n).
     
     INPUT AND OUTPUT:
-        In: Takes a single string containing a piece of text as the first input argument called text. Takes a single integer that determines the number of letters as the second input argument n.
+        In: Takes a single string containing a piece of text as the first input argument called text. 
+            Takes a single integer that determines the number of letters as the second input argument n.
         Out: Returns a text as a single string.
     EXAMPLE:
         In : shuffle_short_words("... Didn't you purchase the e-tickets (and actually 2)?!", 7)
@@ -113,7 +118,8 @@ def shuffle_short_words_re(text, n):
         raise TypeError("The second input argument should be a number")
         
     return re.sub(r"\b\w{,%s}\b" % n, shuffled_word, text)
-    # (if necessary, instead of an upper limit %s in {,%s} one can set a lower limit {%s,} to scramble words longer than n, or both limits to scramble words between certain lengths)
+    # (if necessary, instead of an upper limit %s in {,%s} one can set a lower limit {%s,} to scramble words longer than n, 
+    # or both limits to scramble words between certain lengths)
 
 
 def random_upper(text):
